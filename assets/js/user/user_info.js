1d78a3476dabe2b5fc7ee5d01077ed9dd710430f
wyn <2335627693@qq.com>
2021-01-19 20:32:31 +0800

    个人中心功能完成

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,52 @@
+$(function(){
+    // 定义验证规则
+    var form = layui.form
+    form.verify({
+        nickname:function(value){
+            if(value.length > 6){
+                return "昵称长度为1 ~ 6位之间"
+            }
+        }
+    })
+
+
+    // 用户渲染
+    initUserInfo()
+    function initUserInfo(){
+        $.ajax({
+            method:'GET',
+            url:'/my/userinfo',
+            success:function(res){
+                if(res.status !== 0){
+                    return layui.layer.msg(res.message)
+                }
+                // 成功 渲染
+                form.val('formUserInfo',res.data)
+            }
+        })
+    }
+
+    // 表单重置
+    $('#btnReset').on('click',function(e){
+        e.preventDefault()
+        initUserInfo()
+    })
+
+
+    // 修改用户信息
+    $('.layui-form').on('submit',function(e){
+        e.preventDefault()
+        $.ajax({
+            method:'POST',
+            url:'/my/userinfo',
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status !== 0){
+                    return layui.layer.msg(res.message)
+                }
+                layui.layer.msg(res.message)
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
